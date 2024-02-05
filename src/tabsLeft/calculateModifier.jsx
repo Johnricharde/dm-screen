db74a1c78cefb16c19b718118edffd8b1331b075
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-02-05 13:10:39 +0100

    ternary
    changed the calculateModifier to use ternary operator

--- src/tabsLeft/calculateModifier.jsx
+++ src/tabsLeft/calculateModifier.jsx
@@ -1,7 +1,5 @@
 export default function calculateModifier(abilityScore) {
     let modifier = Math.floor((abilityScore - 10) / 2);
-    if (modifier >= 0) {
-        modifier = "+" + modifier;
-    }
-    return modifier;
+
+    return modifier >= 0 ? "+" + modifier : modifier;
 }
