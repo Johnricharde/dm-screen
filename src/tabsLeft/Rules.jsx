300e762885ea96f301ab0a874b074a2f09e1063d
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-02-12 12:37:52 +0100

    just removed commented code
    removed some old commented out code. the rules search functionality is good enough for now. will move on to the next thing, maybe start working on the SQL server

--- src/tabsLeft/Rules.jsx
+++ src/tabsLeft/Rules.jsx
@@ -19,22 +19,6 @@ export default function Rules() {
 
 
 
-    // function flattenObj(obj) {
-    //     let result = {};
-    //     for (const i in obj) {
-    //         if (typeof obj[i] === "object" && !Array.isArray(obj[i])) {
-    //             const temp = flattenObj(obj[i]);
-    //             for (const j in temp) {
-    //                 result[i + "/" + j] = temp[j];
-    //             }
-    //         } else {
-    //             result[i] = obj[i];
-    //         }
-    //     }
-    //     return result;
-    // }
-
-
     return (
         <div>
             {/* Search bar with autocomplete */}
