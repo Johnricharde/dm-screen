b982cada6d94f813348e033fd084afbe2ca0dd43
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-02-21 09:50:22 +0100

    nit picking

--- frontend/src/tabsLeft/Npcs/Npc.jsx
+++ frontend/src/tabsLeft/Npcs/Npc.jsx
@@ -2,9 +2,9 @@ async function fetchCharacters() {
     try {
         const response = await fetch('http://localhost:3000/api/playerCharacters');
         const data = await response.json();
-        console.log(data);
+        console.log([data]);
     } catch (error) {
-        console.error('||||||||||||||||||||||||||||||Error fetching data: ', error);
+        console.error('Error fetching data: ', error);
     }
 }
 fetchCharacters();
